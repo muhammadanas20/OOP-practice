0fc45221728eb245e1179b91a9b9641140907f9d
muhammadanas20 <muhammadanasashfaq2006@gmail.com>
2026-03-27 11:33:28 +0500

    paper B

---
+++ Lab-mid/final mid prep/paper 1/1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+// Create a class BankAccount to handle unique identification and security.Data Members: * static int nextAccountID: To assign unique IDs.const int accountID: Initialized once and immutable.const string bankName: Static consistency across all objects.double* balance: A pointer to a double representing the balance.Member Initialization List: Use this to initialize all constant members in the constructor.Deep Copy Constructor: Implement a copy constructor that performs a deep copy of the balance pointer but increments the nextAccountID for the new object.Static Function: Provide a function to retrieve the total number of active accounts currently in existence
+class BankAccount{
+    private:
+    
+    public:
+};
+int main(){
+
+}
